caa1551d2336fe8497469b412935ba602a78155d
karthikeyan <karthikeyan@bicsglobal.com>
2025-03-20 17:59:12 +0530

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,63 @@
+# locals {
+#   lambda_files = { for name in var.lambda_names : name => filebase64sha256("${path.module}/../lambda-functions/${name}/package.zip") }
+# }
+
+# resource "aws_lambda_function" "lambda" {
+#   for_each      = local.lambda_files
+#   function_name = each.key
+#   role          = aws_iam_role.lambda_role.arn
+#   handler       = "index.lambda_handler"
+#   runtime       = "python3.8"
+
+#   filename         = "${path.module}/../lambda-functions/${each.key}/package.zip"
+#   source_code_hash = each.value
+#   publish = true
+
+#   environment {
+#     variables = {
+#       ENV = "dev"
+#     }
+#   }
+# }
+
+# resource "aws_iam_role" "lambda_role" {
+#   name = "lambda_execution_role_test"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = { Service = "lambda.amazonaws.com" }
+#     }]
+#   })
+# }
+
+
+
+
+
 locals {
   lambda_files = { for name in var.lambda_names : name => filebase64sha256("${path.module}/../lambda-functions/${name}/package.zip") }
 }
 
+resource "random_string" "suffix" {
+  for_each = local.lambda_files
+  length   = 5
+  special  = false
+  upper    = false
+}
+
 resource "aws_lambda_function" "lambda" {
   for_each      = local.lambda_files
-  function_name = each.key
+  function_name = "${each.key}-${random_string.suffix[each.key].result}"  # Unique per function
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
   runtime       = "python3.8"
 
   filename         = "${path.module}/../lambda-functions/${each.key}/package.zip"
   source_code_hash = each.value
-  publish = true
+  publish          = true  # Ensures a new version is published
 
   environment {
     variables = {
