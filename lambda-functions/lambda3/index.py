b3e3821bd2b086d773727b2d31027445c7f39fc8
karthikeyan <karthikeyan@bicsglobal.com>
2025-03-20 16:56:21 +0530

    modified lambda3

--- lambda-functions/lambda3/index.py
+++ lambda-functions/lambda3/index.py
@@ -3,5 +3,6 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello from lambda3!")
+        # "body": json.dumps("Hello from lambda3!")
+        "body": json.dumps("Updated Lambda Version!")
     }
