02ae6d6f01f79be5b178709aa7c7554a9a6370c5
karthikeyan <karthikeyan@bicsglobal.com>
2025-03-26 14:23:29 +0530

    modified iam name

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "lambda" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role_test"
+  name = "lambda_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
