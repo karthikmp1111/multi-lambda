57edf3c0b4186bd79a74a19b1af73f52e1c8d527
karthikeyan <karthikeyan@bicsglobal.com>
2025-03-31 15:45:38 +0530

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,9 +51,66 @@
 #     for name in var.lambda_names : name => "s3://bg-kar-terraform-state/lambda-packages/${name}/package.zip"
 #   }
 # }
+
+# resource "aws_lambda_function" "lambda" {
+#   for_each      = local.lambda_files
+#   function_name = each.key
+#   role          = aws_iam_role.lambda_role.arn
+#   handler       = "index.lambda_handler"
+#   runtime       = "python3.8"
+
+#   # Directly specify the S3 bucket and key for the Lambda function package
+#   s3_bucket = "bg-kar-terraform-state"
+#   s3_key    = "lambda-packages/${each.key}/package.zip"
+
+#   # If you want to hash the S3 package to track changes
+#   source_code_hash = each.value
+
+#   publish          = true
+
+#   environment {
+#     variables = {
+#       ENV = "dev"
+#       NEW_VARIABLE = "bg_lambda_test"
+#     }
+#   }
+
+#   lifecycle {
+#     ignore_changes = [environment, publish]  # Ignores changes in environment variables & publish flag
+#   }
+# }
+
+# resource "aws_iam_role" "lambda_role" {
+#   name = "bg_lambda_execution_role"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = { Service = "lambda.amazonaws.com" }
+#     }]
+#   })
+
+#   lifecycle {
+#     ignore_changes = [name]  # Prevents unnecessary IAM role recreation
+#   }
+# }
+
+
 locals {
-  lambda_files = { for name in var.lambda_names : name => filebase64sha256("s3://$S3_BUCKET/lambda-packages/${name}/package.zip") }
+  lambda_files = {
+    for name in var.lambda_names : name => "s3://bg-kar-terraform-state/lambda-packages/${name}/package.zip"
+  }
 }
+
+# Fetch the S3 objects for the Lambda packages to track changes via their ETag
+data "aws_s3_object" "lambda_package" {
+  for_each = var.lambda_names
+  bucket   = "bg-kar-terraform-state"
+  key      = "lambda-packages/${each.key}/package.zip"
+}
+
 resource "aws_lambda_function" "lambda" {
   for_each      = local.lambda_files
   function_name = each.key
@@ -65,8 +122,8 @@ resource "aws_lambda_function" "lambda" {
   s3_bucket = "bg-kar-terraform-state"
   s3_key    = "lambda-packages/${each.key}/package.zip"
 
-  # If you want to hash the S3 package to track changes
-  source_code_hash = each.value
+  # Use the ETag (source_code_hash) of the S3 object to detect changes
+  source_code_hash = data.aws_s3_object.lambda_package[each.key].etag
 
   publish          = true
 
