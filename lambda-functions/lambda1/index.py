9468cd7989f3f2b9a36606b06c934ca339944f24
karthikeyan <karthikeyan@bicsglobal.com>
2025-04-09 15:38:05 +0530

    added files in lambda

--- lambda-functions/lambda1/index.py
+++ lambda-functions/lambda1/index.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello from lambda1 bg!")
+        "body": json.dumps("Hello from lambda 1 bg!")
     }
